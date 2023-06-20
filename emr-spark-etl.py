82f8bf28ebc3c8329d990e8180a62f5f8a2b8051
mlprojectjyo <136455339+mlprojectjyo@users.noreply.github.com>
2023-06-20 14:17:09 +0530

    Update emr-spark-etl.py
    updating indentation error for file

--- emr-spark-etl.py
+++ emr-spark-etl.py
@@ -6,24 +6,24 @@ from pyspark.sql.functions import *
 
 if __name__ == "__main__":
 
-        print(len(sys.argv))
-            if (len(sys.argv) != 3):
-                        print("Usage: spark-etl-car-company [input-folder] [output-folder]")
-                                sys.exit(0)
+    print(len(sys.argv))
+    if (len(sys.argv) != 3):
+        print("Usage: spark-etl-car-company [input-folder] [output-folder]")
+        sys.exit(0)
 
-                                    spark = SparkSession\
-                                                    .builder\
-                                                            .appName("EMRSparkETLCarCompany")\
-                                                                    .getOrCreate()
+    spark = SparkSession\
+        .builder\
+        .appName("EMRSparkETLCarCompany")\
+        .getOrCreate()
 
-                                                                        CarCompany = spark.read.option("inferSchema", "true").option("header", "true").csv(sys.argv[1])
+    CarCompany = spark.read.option("inferSchema", "true").option("header", "true").csv(sys.argv[1])
 
-                                                                            updatedCarCompany = CarCompany.withColumn("current_date", lit(datetime.now()))
+    updatedCarCompany = CarCompany.withColumn("current_date", lit(datetime.now()))
 
-                                                                                updatedCarCompany.printSchema()
+    updatedCarCompany.printSchema()
 
-                                                                                    print(updatedCarCompany.show())
+    print(updatedCarCompany.show())
 
-                                                                                        print("Total number of records: " + str(updatedCarCompany.count()))
+    print("Total number of records: " + str(updatedCarCompany.count()))
 
-                                                                                            updatedCarCompany.write.parquet(sys.argv[2])
+    updatedCarCompany.write.parquet(sys.argv[2])
